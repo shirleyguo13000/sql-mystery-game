b7500b2d88624cfab9e15a22738360061392bc0d
shirleyguo13000 <shirleyguo13000@gmail.com>
2026-06-15 03:31:17 -0400

    Update murder-mystery-game.sql

--- murder-mystery-game.sql
+++ murder-mystery-game.sql
@@ -65,45 +65,76 @@ WHERE event_name = 'The Funky Grooves Tour';
 -- 67318	    4719	     The Funky Grooves Tour	   20180115
 -- who is the third person?
 
+
 SELECT *
 FROM person
 WHERE id = 67318;
 -- id	    name	           license_id	  address_number	address_street_name	     ssn
 -- 67318	Jeremy Bowers	   423327	      530	            Washington Pl, Apt 3A	   871539279
+SELECT *
+FROM drivers_license
+WHERE id = 423327;
+-- id	     age	  height	  eye_color	  hair_color	 gender	  plate_number	 car_make	    car_model
+-- 423327	 30	    70	      brown	      brown	       male   	0H42W2	       Chevrolet	  Spark LS
+SELECT *
+FROM income
+WHERE ssn = '871539279';
+-- ssn	        annual_income
+-- 871539279	  10500
+-- JEREMY IS THE ONLY ONE WHOSE SSN SHOWS UP IN THE INCOME SECTION
+
+-- interview of jeremy's statement
+SELECT *
+FROM interview
+where person_id = 67318;
+-- ID=67318	
+-- I was hired by a woman with a lot of money. 
+-- I don't know her name but I know she's around 5'5" (65") or 5'7" (67"). 
+-- She has red hair and she drives a Tesla Model S. I know that she attended the SQL Symphony Concert 3 times in December 2017.
 
 
+SELECT *
+FROM facebook_event_checkin
+where event_name = 'SQL Symphony Concert'
+AND date LIKE '201712%%'
+AND person_id = 67318;
+-- person_id	event_id	event_name	date
+-- 67318	1143	SQL Symphony Concert	20171206
 
-SELECT * FROM get_fit_now_check_in
-WHERE check_in_date = 20180115;
--- membership_id	check_in_date	check_in_time	check_out_time
--- D2KY6	20180115	746	836
--- 344VM	20180115	1087	1195
--- 3BRSC	20180115	354	825
--- HM6U8	20180115	525	800
+SELECT *
+FROM facebook_event_checkin
+where event_name = 'SQL Symphony Concert'
+AND date ='20171206';
+-- person_id	event_id	event_name	          date
+-- 69325	    1143	    SQL Symphony Concert	20171206
+-- 99716	    1143	    SQL Symphony Concert	20171206
+-- 67318	    1143	    SQL Symphony Concert	20171206
 
-SELECT * FROM get_fit_now_member
-WHERE id = 'D2KY6';
--- id	person_id	name	membership_start_date	membership_status
--- D2KY6	99602	Joline Hollering	20170220	gold
-
-SELECT * FROM get_fit_now_member
-WHERE id = '344VM';
--- id	person_id	name	membership_start_date	membership_status
--- 344VM	13466	Armando Huie	20171002	regular
-
-SELECT * FROM get_fit_now_member
-WHERE id = '3BRSC';
--- id	person_id	name	membership_start_date	membership_status
--- 3BRSC	19948	Taylor Skyes	20180314	gold
-
-SELECT * FROM get_fit_now_member
-WHERE id = 'HM6U8';
--- id	person_id	name	membership_start_date	membership_status
--- HM6U8	50106	Edgar Bamba	20170128	silver
+-- find out who the other two are!
+SELECT *
+FROM person
+where id = 99716;
+-- id	    name	            license_id	address_number	address_street_name	  ssn
+-- 99716	Miranda Priestly	202298	    1883	          Golden Ave	          987756388
+SELECT *
+FROM drivers_license
+WHERE id = 202298;
+-- id	     age	height	eye_color	 hair_color	gender	plate_number	car_make	 car_model
+-- 202298	  68	66	     green	   red	      female	500123	      Tesla	      Model S
+SELECT *
+FROM income
+WHERE ssn = '987756388';
+-- ssn	      annual_income
+-- 987756388	310000
 
 
+INSERT INTO solution VALUES (1, 'Miranda Priestly');
+        
+        SELECT value FROM solution;
 
-
+-- Congrats, you found the brains behind the murder!
+-- Everyone in SQL City hails you as the greatest SQL detective of all time.
+-- Time to break out the champagne!
 
 
 
