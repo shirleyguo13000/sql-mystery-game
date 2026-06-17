ae6a70e367ca8a8f02dd4f8d4ee4ee5da318530d
shirleyguo13000 <shirleyguo13000@gmail.com>
2026-06-17 16:43:17 -0400

    Update sql-noir-4-vanishing-diamond.sql

--- sql-noir-4-vanishing-diamond.sql
+++ sql-noir-4-vanishing-diamond.sql
@@ -2,3 +2,7 @@
   -- 1. The SQL queries you ran
   -- 2. Any notes or insights as SQL comments
   -- 3. Your final conclusion: who did it?
+
+-- HI, I did all the work and then pressed back page instead of commit changes.
+-- the good news is, I found out who the theif was.
+-- It's Mike Manning.
